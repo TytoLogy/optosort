6d1881f45e12def6b5c9fdacfd50a1b5fb271edf
sshanbhag <sshanbhag@neomed.edu>
2020-02-26 13:34:52 -0500

    moved test_buildcache to opto

--- test_buildcache.m
+++
@@ -1,8 +0,0 @@
-test = Dinf.test;
-test.audio = Dinf.audio;
-test.opto = Dinf.opto;
-tdt.outdev = Dinf.outdev;
-test.audio.signal.Type = char(Dinf.audio.signal.Type);
-
-
-[stimcache, stimseq] = opto_buildStimCache(test, tdt, Dinf.caldata);
