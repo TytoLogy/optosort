a42e4e2ce3c0b60243de17199dc63f34a8d6d9d8
sshanbhag <sshanbhag@neomed.edu>
2020-03-05 14:53:37 -0500

    functionalized extract_snippets

--- extract_snippets.m
+++ extract_snippets.m
@@ -1,4 +1,4 @@
-%function snips = extract_snippets(snipwin, timestamps, sweeps, Fs)
+function snippets = extract_snippets(timestamps, sweeps, snipwin, Fs)
 
 % timestamps is a nreps X 1 cell array with each row containing a list
 % (vector) of timestamps, in milliseconds
@@ -11,6 +11,7 @@ timestamps = spiketimes{1};
 sweeps = tracesByStim{1};
 Fs = cInfo.Dinf.indev.Fs;
 
+%% check sizes of input arrays
 [tsrows, tscols] = size(timestamps);
 [sweeprows, sweepcols] = size(sweeps);
 
@@ -32,46 +33,55 @@ rpad = zeros(1, snipbin(2));
 ctrbin = snipbin(1) + 1;
 
 %% test algorithm on one sweep
-snippets = cell(sweeprows, 1);
-for r = sweeprows
-	
-	ts = timestamps{sweeprows}(1)
-	tsbin = ms2bin(ts, Fs)
 
+% allocate storage for snippets
+snippets = cell(sweeprows, 1);
+% loop through traces
+for r = sweeprows
+	% plot sweep
 	plot(1:sweepcols, sweeps(r, :), 'c.-');
 	grid on
-	hold on
-		plot(tsbin, sweeps(r, tsbin), 'or')
-	hold off
-	drawnow
 	
 	% idea 1: by default, pad the left and right of the sweep
 	padsweep = [lpad sweeps(r, :) rpad];
+	% plot the padded data - need to offset the xaxis by length of lpad
 	hold on
-		plot([1:length(padsweep)]-snipbin(1), padsweep, 'k.')
+		plot( (1:length(padsweep))-snipbin(1), padsweep, 'k.')
 	hold off
 	axis tight
+
+	% loop through timestamps
+	for t = 1:length(timestamps{r})
+		ts = timestamps{r}(t);
+		tsbin = ms2bin(ts, Fs);
+
+		% highlight timestamp
+		hold on
+			plot(tsbin, sweeps(r, tsbin), 'or')
+		hold off
 	
-	% get snippet start and end bins - need to account for the shift due to
-	% lpad!
-	% this is the explicit routine: 
-	% for the start bin, take original bin, apply the lpad offset
-	% stored in snipbin(1) and then subtract the lpad bins to capture the
-	% desired "pre-timestamp" waveform.
-	% for the end bin, shift the timestamp bin by lpad length (snipbin(1))
-	% and then add the post-timestamp bins (snipbin(2))
-	% 	
-	% 	snip_startbin = ((tsbin + snipbin(1)) - snipbin(1))
-	% 	snip_endbin = ((tsbin + snipbin(1)) + snipbin(2))
-	% 	
-	% but we can simply use the original tsbin, since the padded sweep is
-	% already shifted by snipbin(1)!!!
-	snip_startbin = tsbin
-	snip_endbin = ((tsbin + snipbin(1)) + snipbin(2))
-	snip = padsweep( snip_startbin:snip_endbin);
-	
-	hold on
-		plot((snip_startbin:snip_endbin) - snipbin(1), snip, 'g.:')
-	hold off
-	
+		% get snippet start and end bins - need to account for the shift due to
+		% lpad!
+		% this is the explicit routine: 
+		% for the start bin, take original bin, apply the lpad offset
+		% stored in snipbin(1) and then subtract the lpad bins to capture the
+		% desired "pre-timestamp" waveform.
+		% for the end bin, shift the timestamp bin by lpad length (snipbin(1))
+		% and then add the post-timestamp bins (snipbin(2))
+		% 	
+		% 	snip_startbin = ((tsbin + snipbin(1)) - snipbin(1))
+		% 	snip_endbin = ((tsbin + snipbin(1)) + snipbin(2))
+		% 	
+		% but we can simply use the original tsbin, since the padded sweep is
+		% already shifted by snipbin(1)!!!
+		snip_startbin = tsbin
+		snip_endbin = ((tsbin + snipbin(1)) + snipbin(2))
+		snip = padsweep( snip_startbin:snip_endbin);
+
+		hold on
+			plot((snip_startbin:snip_endbin) - snipbin(1), snip, 'g.:')
+		hold off
+		drawnow
+		pause
+	end
 end	
