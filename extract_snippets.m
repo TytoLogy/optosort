b930417339c37a65dccb26b60eef65575518ee15
sshanbhag <sshanbhag@neomed.edu>
2020-03-05 10:58:54 -0500

    Update extract_snippets.m

--- extract_snippets.m
+++ extract_snippets.m
@@ -1,4 +1,4 @@
-%function snips = extract_snippets(timestamps, sweeps)
+%function snips = extract_snippets(snipwin, timestamps, sweeps, Fs)
 
 % timestamps is a nreps X 1 cell array with each row containing a list
 % (vector) of timestamps, in milliseconds
@@ -9,22 +9,44 @@
 load snipindata.mat
 timestamps = spiketimes{1};
 sweeps = tracesByStim{1};
+Fs = cInfo.Dinf.indev.Fs;
 
 [tsrows, tscols] = size(timestamps);
 [sweeprows, sweepcols] = size(sweeps);
 
-% check to make sure sweeps is in proper orientation
-if sweeprows < sweepcols
-	sweeps = sweeps'
+% check to make sure sweeps is in proper orientation (samples in columns,
+% trials/sweeps in rows)
+if sweeprows > sweepcols
+	sweeps = sweeps';
+	[sweeprows, sweepcols] = size(sweeps);
+end
+% make sure # of rows match in the two arrays
+if sweeprows ~= tsrows
+	error('%s: mismatch in # of rows in sweeps and timestamps', mfilename);
+end
+% pre-ts post-ts window
+snipwin = [1 2];
+snipbin = ms2bin(snipwin, Fs)
+lpad = zeros(1, snipbin(1));
+rpad = zeros(1, snipbin(2));
+ctrbin = snipbin(1) + 1;
 
-%%
-snippets = cell(nr, nc);
-for r = 1:nr
-	for c = 1:nc
-		for n = 1:cInfo.nreps
-				tracesByStim{r, c}(:, n) = filtfilt(BPfilt.b, BPfilt.a, ...
-															tracesByStim{r, c}(:, n));
-
-		end
-	end
+%% test algorithm on one sweep
+snippets = cell(sweeprows, 1);
+for r = sweeprows
+	ts = timestamps{sweeprows}(1)
+	tsbin = ms2bin(ts, Fs)
+	plot(1:sweepcols, sweeps(r, :));
+	grid on
+	hold on
+		plot(tsbin, sweeps(r, tsbin), 'or')
+	hold off
+	drawnow
+	
+	% idea 1: by default, pad the left and right of the sweep
+	padsweep = [lpad sweeps(r, :) rpad];
+	hold on
+		plot([1:length(padsweep)]-snipbin(1), padsweep, 'g.')
+	hold off
+	axis tight
 end	
