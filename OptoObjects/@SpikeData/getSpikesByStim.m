5b0e1d5f214262f2bdd210c16d252bf7c2f5961c
sshanbhag <sshanbhag@neomed.edu>
2020-10-03 19:58:15 -0400

    Update getSpikesByStim.m
    reworked output to have either a full spikes struct or a reduced spikes struct in addition to info_struct. This reduces redundant information in fra/ftc spike data mat files for use with CF/BF calculations in APANanalyze.
    
    Reworked comments as well
    
    Should function as before in most cases.

--- OptoObjects/@SpikeData/getSpikesByStim.m
+++ OptoObjects/@SpikeData/getSpikesByStim.m
@@ -1,8 +1,7 @@
 function varargout = getSpikesByStim(obj, fIndx, chanNum, unitNum)
+% [spike_struct, info_struct] = getSpikesByStim(fIndx, chanNum, unitNum)
 %------------------------------------------------------------------------
-% [data, datainfo, tracesByStim] = viewOptoData(varargin)
-%------------------------------------------------------------------------
-% TytoLogy:opto
+% TytoLogy:optosort:OptoObjects
 %--------------------------------------------------------------------------
 %	method: SpikeData.getSpikesByStim
 %--------------------------------------------------------------------------
@@ -14,12 +13,69 @@ function varargout = getSpikesByStim(obj, fIndx, chanNum, unitNum)
 %	CurveInfo/WAVInfo/FRAInfo.convertSpikeTableToSpikeTimes
 %	
 %------------------------------------------------------------------------
+% Input Arguments:
+%   fIndx    index to SpikeData.FileInfo{} cell for desired test
+% 	 chanNum  A/D channel for requested data
+% 	 unitNum  unit id number (from sorting program) 
+%
 % Output Arguments:
+% if only 1 output requested, spike_struct will be returned:
+%  spike_struct	output struct. organization of elements within the struct will
+%                 vary depending on test type (particularly for FRA), but the 
+%                 fields are (this is for FRA):
+% 				spiketimes   {# levels, # frequencies} cell array, each element is
+% 								 an {nreps, 1} cell array of spiketimes (in milliseconds, 
+% 								 relative to start of sweep) for each rep of this stimulus
+% 				stimindex    {# levels, # frequencies} cell array, each element is
+% 								 an [nreps, 1] vector of indices into spiketable{} 
+% 								 for each instance of this stimulus combination
+% 				stimvar      { [# frequencies, 1], [# levels, 1]} array of stimulus 
+% 								 frequency and level (dB SPL) values for each of the
+% 								 elements in spiketimes and stimindex. This really should be
+% 								 in order of { levels, freqs} ...
+% 				unique_stim  {2, 1} cell, simply vectors of frequency and level
+% 				nstim        [2, 1] array, with # of freqs, levels
+% 				spiketable   {total # stimulus combinations * # reps, 1} cell array
+% 								 with elements from the SpikeData.Spikes table for this
+% 								 unit's spikes for each stimulus. Has waveforms for spikes
+% 				fileIndex    index into SpikeData.FileInfo{} cell of file/test
+% 								 information
+% 				channel      A/D channel
+% 				unit         unit ID #
+% 				fileName	    source raw .dat file from opto program
 %
-%
+% With two output arguements, both spike_struct and info_struct will be
+% returned. Fields in the structs will be different:
+% 
+% 		spike_struct	output struct with spike information/data
+% 			Fields (this is for FRA data):
+% 				spiketimes   {# levels, # frequencies} cell array, each element is
+% 								 an {nreps, 1} cell array of spiketimes (in milliseconds, 
+% 								 relative to start of sweep) for each rep of this stimulus
+% 				spiketable   {total # stimulus combinations * # reps, 1} cell array
+% 								 with elements from the SpikeData.Spikes table for this
+% 								 unit's spikes for each stimulus. Has waveforms for spikes
+% 				channel      A/D channel
+% 				unit         unit ID #
+% 
+% 		info_struct	output struct with information about test
+% 			Fields (for FRA data):
+% 				stimindex    {# levels, # frequencies} cell array, each element is
+% 								 an [nreps, 1] vector of indices into spiketable{} 
+% 								 for each instance of this stimulus combination
+% 				stimvar      { [# frequencies, 1], [# levels, 1]} array of stimulus 
+% 								 frequency and level (dB SPL) values for each of the
+% 								 elements in spiketimes and stimindex. This really should be
+% 								 in order of { levels, freqs} ...
+% 				unique_stim  {2, 1} cell, simply vectors of frequency and level
+% 				nstim        [2, 1] array, with # of freqs, levels
+% 				fileIndex    index into SpikeData.FileInfo{} cell of file/test
+% 								 information
+% 				fileName	    source raw .dat file from opto program
 %------------------------------------------------------------------------
-% See Also:
+% See Also: SpikeData, SpikeInfo
 %------------------------------------------------------------------------
+
 %------------------------------------------------------------------------
 %  Sharad Shanbhag
 %   sshanbhag@neomed.edu
@@ -34,6 +90,9 @@ function varargout = getSpikesByStim(obj, fIndx, chanNum, unitNum)
 %		- moved into method for SpikeData
 %	24 Jan 2020 (SJS): moved different methods for each curve type
 %	(subclass) into its respective file
+%  3 Oct 2020 (SJS): mods for APANalyze, future analysis
+%   - if looking across units, there is redundant information in the str
+%     struct. modified output behavior to deal with this
 %------------------------------------------------------------------------
 % TO DO:
 %   *Documentation!
@@ -119,12 +178,40 @@ spiketable = obj.spikesForAnalysis(fIndx, 'channel', chanNum, ...
 %------------------------------------------------------------------------
 str = obj.Info.FileInfo{fIndx}.convertSpikeTableToSpikeTimes(spiketable);
 
-% add fields to str
-str.fileIndex = fIndx;
-str.channel = chanNum;
-str.unit = unitNum;
-str.fileName = obj.Info.FileInfo{fIndx}.F.file;
-% assign output
-varargout{1} = str;
+if nargout == 1
+	% everything goes in spike_struct (varargout{1})
+	% add fields to str
+	str.fileIndex = fIndx;
+	str.channel = chanNum;
+	str.unit = unitNum;
+	str.fileName = obj.Info.FileInfo{fIndx}.F.file;
+	% assign output
+	varargout{1} = str;
+else
+	%{
+	varargout{1} = struct(	'spiketimes', str.spiketimes, ...
+									'spiketable', str.spiketable, ...
+									'channel', chanNum, ...
+									'unit', unitNum );
+	varargout{2} = struct(	'stimindex', str.stimindex, ...
+									'stimvar', str.stimvar, ...
+									'unique_stim', str.unique_stim, ...
+									'nstim', str.nstim, ...
+									'fileIndex', fIndx, ...
+									'fileName', obj.Info.FileInfo{fIndx}.F.file);
+	%}
+	% assign spike_struct fields to varargout{1}
+	varargout{1}.spiketimes = str.spiketimes;
+	varargout{1}.spiketable = str.spiketable;
+	varargout{1}.channel = chanNum;
+	varargout{1}.unit = unitNum;
+	% assign info_struct to varargout{2}
+	varargout{2}.stimindex = str.stimindex;
+	varargout{2}.stimvar = str.stimvar;
+	varargout{2}.unique_stim = str.unique_stim;
+	varargout{2}.nstim = str.nstim;
+	varargout{2}.fileIndex = fIndx;
+	varargout{2}.fileName = obj.Info.FileInfo{fIndx}.F.file;
 
-	
+end
+									
