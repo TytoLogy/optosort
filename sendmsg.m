e22c17c1a6d14bd7dea334f1bf4742f3274eac17
Sharad Shanbhag <sshanbhag@neomed.edu>
2021-08-03 00:13:41 -0400

    Update sendmsg.m
    didn't push changes to this on myotis- will need to resolve diffs

--- sendmsg.m
+++ sendmsg.m
@@ -15,7 +15,23 @@ else
 	sepchar = '-';
 end
 % sepstr = '----------------------------------------------------';
-sepstr = repmat(sepchar, 1, length(msgstr) + 2); 
 
-fprintf('%s\n%s\n%s\n', sepstr, msgstr, sepstr);
+if iscell(msgstr)
+   nchar = zeros(length(msgstr), 1);
+   for n = 1:length(msgstr)
+      nchar(n) = length(msgstr{n});
+   end
+   sepstr = repmat(sepchar, 1, max(nchar) + 2);
+   
+   fprintf('%s\n', sepstr);
+   for n = 1:length(msgstr)
+      fprintf('%s\n', msgstr{n});
+   end
+   fprintf('%s\n', sepstr);
+         
+else
+   sepstr = repmat(sepchar, 1, length(msgstr) + 2);
+   fprintf('%s\n%s\n%s\n', sepstr, msgstr, sepstr);
+
+end
 
