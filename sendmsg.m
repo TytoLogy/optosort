bfa48770e70fef001f03fa11854f50f645c2d9db
Sharad Shanbhag <sshanbhag@neomed.edu>
2021-08-03 12:04:53 -0400

    Update sendmsg.m
    added multiline (via cell input)

--- sendmsg.m
+++ sendmsg.m
@@ -1,21 +1,43 @@
 function sendmsg(msgstr, varargin)
 %------------------------------------------------------------------------
-% sendmsg(msgstr)
+% sendmsg(msgstr, sepchar)
 %------------------------------------------------------------------------
-% displays message between two separation strings of dashes
+% displays message between two separation strings of dashes (or other
+% character given by sepchar
 %------------------------------------------------------------------------
 % Input Arguments:
 % 	msgstr		string to display
+%  
+%  Optional Inputs:
+%   sepchar    alternative "separation" character (default is '-')
+%
 % Output Arguments:
 % 	none
 %------------------------------------------------------------------------
+
 if nargin > 1
 	sepchar = varargin{1};
 else
 	sepchar = '-';
 end
-% sepstr = '----------------------------------------------------';
-sepstr = repmat(sepchar, 1, length(msgstr) + 2); 
 
-fprintf('%s\n%s\n%s\n', sepstr, msgstr, sepstr);
+% sepstr = '----------------------------------------------------';
+
+if iscell(msgstr)
+   len = zeros(size(msgstr));
+   for n = 1:length(msgstr)
+      len(n) = length(msgstr{n});
+   end
+   sepstr = repmat(sepchar, 1, max(len) + 2); 
+
+   fprintf('%s\n', sepstr);
+   for n = 1:length(msgstr)
+      fprintf('%s\n', msgstr{n});
+   end
+   fprintf('%s\n', sepstr);   
+else
+   sepstr = repmat(sepchar, 1, length(msgstr) + 2); 
+   fprintf('%s\n%s\n%s\n', sepstr, msgstr, sepstr);
+end
+
 
